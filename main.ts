75d88e730c573267e0fa9a5063ce02f0d7df402f
Cutfiregamer <133775047+Cutfiregamer@users.noreply.github.com>
2023-05-19 09:24:48 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,51 +1,33 @@
-basic.showString("Este puzle es aún más dificil que el anterior :)")
-basic.pause(5000)
-basic.showLeds(`
-    . . . . #
-    . . . # #
-    . . # # #
-    . # # # #
-    # # # # #
-    `)
-basic.forever(function () {
-    basic.pause(1000)
-    if (input.buttonIsPressed(Button.A)) {
-        if (input.logoIsPressed()) {
-            if (input.isGesture(Gesture.TiltRight)) {
-                if (input.buttonIsPressed(Button.B)) {
-                    if (input.lightLevel() <= 30) {
-                        music.playMelody("B A G A G F A C5 ", 120)
-                        basic.pause(5000)
-                    } else {
-                        while (input.lightLevel() > 30) {
-                            basic.showString("Mucha luz")
-                        }
-                    }
-                } else {
-                    basic.showLeds(`
-                        # # # . .
-                        # . . # .
-                        # # # . .
-                        # . . # .
-                        # # # . .
-                        `)
-                }
-            } else {
-                while (!(input.isGesture(Gesture.TiltRight))) {
-                    basic.showIcon(IconNames.No)
-                    basic.showArrow(ArrowNames.West)
-                }
-            }
-        } else {
-            basic.showArrow(ArrowNames.North)
-        }
-    } else {
-        basic.showLeds(`
-            . . . . #
-            . . . # #
-            . . # # #
-            . # # # #
-            # # # # #
-            `)
+// cambiar algunas cosas
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (resultado > Tiempo) {
+        music.playMelody("G B A G C5 B A B ", 120)
+        basic.showString("Jugador A gana")
+    }
+    if (resultado < Tiempo) {
+        music.playMelody("B A G A G F A C5 ", 120)
+        basic.showString("Jugador B gana")
     }
 })
+input.onButtonPressed(Button.A, function () {
+    if (Tiempo == 2) {
+        Tiempo += 0
+    } else if (Tiempo == 0) {
+        Tiempo += 1
+    } else {
+        Tiempo += 2
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (resultado == 2) {
+        resultado += 0
+    } else if (resultado == 0) {
+        resultado += 1
+    } else {
+        resultado += 2
+    }
+})
+let Tiempo = 0
+let resultado = 0
+resultado = 0
+Tiempo = 0
